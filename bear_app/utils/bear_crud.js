fdbce3bcf60483f957489688dd91d6720efa6651
Ivan Ivic <iivic@extensionengine.com>
2017-07-05 13:48:27 +0200

    Code cleanup

---
+++ bear_app/utils/bear_crud.js
@@ -0,0 +1,55 @@
+var Bear = require('../models/bear');
+
+module.exports.createBear = function(req, res) {
+    new Bear(req.body)
+    .save()
+    .then(function(bear) {
+        res.json({response: bear});
+    })
+    .catch(function(err) {
+        res.send(err);
+    });
+};
+
+module.exports.getAllBears = function(_, res) {
+    Bear.fetchAll()
+    .then(function(bears) {
+        res.json({response: bears});
+    })
+    .catch(function(err) {
+        res.send(err);
+    });
+};
+
+module.exports.getOneBear = function(req, res) {
+    Bear.where(req.params)
+    .fetch()
+    .then(function(bear) {
+        res.json({response: bear});
+    })
+    .catch(function(err) {
+        res.send(err);
+    });
+};
+
+module.exports.updateBear = function(req, res) {
+    new Bear(req.params)
+    .save(req.body)
+    .then(function(bear) {
+        res.json({response: bear});
+    })
+    .catch(function(err) {
+        res.send(err);
+    });
+};
+
+module.exports.deleteBear = function(req, res) {
+    new Bear(req.params)
+    .destroy({require: true})
+    .then(function() {
+        res.json({message: 'Successfully deleted bear.'});
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+};
--- server.js
+++ server.js
@@ -1,26 +1,16 @@
 // server.js
 require('dotenv').config();
 
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
-var express = require('express'),
-    app = express(),
+var app,
     bodyParser = require('body-parser'),
     port = process.env.PORT,
-    router = require('./bear_app/routes/bear_urls')
+    Router = require('./bear_app/routes/bear_urls');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+app = require('express')()
+.use(bodyParser.urlencoded({extended: true}))
+.use(bodyParser.json())
+.use('/api', Router)
+.listen(port, function(err) {
+    if (err) console.error('Error: Starting server failed,', err.message);
+    console.log('Listening on port ' + port);
+});
